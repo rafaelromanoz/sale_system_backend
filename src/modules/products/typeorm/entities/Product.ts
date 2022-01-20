558ae9bd6cbe8614c206707631c58d254130ba8a
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-01-20 12:27:38 -0300

    :sparkles: implementing orders_products migrations

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -9,14 +9,19 @@ import {
 class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   name: string;
+
   @Column('decimal')
   price: number;
+
   @Column('int')
   quantity: number;
+
   @CreateDateColumn()
   created_at: Date;
+
   @CreateDateColumn()
   updated_at: Date;
 }
